9abb3e7c79df1e51510ef7e0ca7f28cc6f80d014
Evoloxi <evoloxix@gmail.com>
2024-10-26 18:41:37 +0200

    Update imports to use esp-idf-svc consistently

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,7 @@
 use crate::{arc_mutex, info};
 use embedded_svc::http::Method;
 use embedded_svc::io::Write;
-use esp_idf_hal::sys::{esp, esp_wifi_get_ps, esp_wifi_set_ps, wifi_ps_type_t, wifi_ps_type_t_WIFI_PS_MAX_MODEM, wifi_ps_type_t_WIFI_PS_NONE};
+use esp_idf_svc::sys::{esp, esp_wifi_get_ps, esp_wifi_set_ps, wifi_ps_type_t, wifi_ps_type_t_WIFI_PS_MAX_MODEM, wifi_ps_type_t_WIFI_PS_NONE};
 use esp_idf_svc::http::server::EspHttpServer;
 use esp_idf_svc::timer::EspTaskTimerService;
 use esp_idf_svc::wifi::EspWifi;
